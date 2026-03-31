6f10b92aac8e07507c8d4454ae38e8c4e44b4a84
abbosbek2006 <abbosbek2006@gmail.com>
2026-03-31 05:58:26 +0500

    fix: correct index file

--- sw.js
+++ sw.js
@@ -1,23 +1,26 @@
-const CACHE_NAME = "aquarium-v3";
+const CACHE_NAME = "aquarium-v10";
 
 const ASSETS = [
   "/",
   "/index.html",
   "/style.css",
   "/script.js",
-  "/offline.html"
+  "/offline.html",
 ];
 
 self.addEventListener("install", (e) => {
+  self.skipWaiting();
+
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
-    })
+    }),
   );
 });
 
-
 self.addEventListener("activate", (e) => {
+  clients.claim();
+
   e.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(
@@ -25,26 +28,23 @@ self.addEventListener("activate", (e) => {
           if (key !== CACHE_NAME) {
             return caches.delete(key);
           }
-        })
-      )
-    )
+        }),
+      ),
+    ),
   );
 });
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => {
-      return (
-        res ||
-        fetch(e.request)
-          .then((response) => {
-            return caches.open(CACHE_NAME).then((cache) => {
-              cache.put(e.request, response.clone());
-              return response;
-            });
-          })
-          .catch(() => caches.match("/offline.html"))
-      );
-    })
+    fetch(e.request)
+      .then((response) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => {
+        return caches.match(e.request) || caches.match("/offline.html");
+      }),
   );
 });
